69d3f280a3f401c7c54dd9fc706e730214037bc6
Justin Curl <jcurl@princeton.edu>
2020-11-11 13:27:21 -0800

    .

--- notifier.py
+++ notifier.py
@@ -82,19 +82,26 @@ def notify():
     
     try:
       print(s_subject.split())
+      print("".join(s_subject.split()))
     except:
       print('subject splitting error')
 
     try:
-      print(s_subject.replace(" ", ""))
+      print("whitespace removal" + s_subject.replace(" ", ""))
     except:
       print('whitespace replacement error')
+
+    try:
+      print(s_enrollment.split())
+    except:
+      print('enrollment splitting error')
   # Create string version of message
     msg_info += "\nClass Information:{}\n".format(s_subject)
     for i in range(len(text)):
       msg_info += "Class Number: {}\nSection: {}\nEnrollment:\n{}".format(text[i][0].get_text(), text[i][1].get_text(), text[i][2].get_text())
 
     print('text: ', msg_info)
+  
   # Turn html into MIMEText objects
   part1 = MIMEText(msg_info, "plain")
 
