718135035f0c1361538fb4fb40a72da9f391d94f
Justin Curl <jcurl@princeton.edu>
2020-11-11 09:50:22 -0800

    put inside for-loop

--- practicecron.py
+++ practicecron.py
@@ -12,76 +12,76 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-driver.get('https://registrar.princeton.edu/course-offerings/course-details?term=1214&courseid=005300')
+url_mappings = {
+  'POL316': 'https://registrar.princeton.edu/course-offerings/course-details?term=1214&courseid=005300',
+  'POL423': 'https://registrar.princeton.edu/course-offerings/course-details?term=1214&courseid=009187',
+  'POL563': 'https://registrar.princeton.edu/course-offerings/course-details?term=1214&courseid=005433',
+  }
+courses = ['POL316', 'POL423', 'POL563']
+for course in courses:
+  driver.get(url_mappings[course])
 
-driver.save_screenshot('screenshot.png')
+  page = driver.page_source
 
-page = driver.page_source
+  soup = BeautifulSoup(page, 'html.parser')
 
-soup = BeautifulSoup(page, 'html.parser')
+  print("============ SOUP ==========")
+  print(soup.prettify())
 
-print("============ SOUP ==========")
-print(soup.prettify())
+  results = soup.find(id='course-details')
 
-results = soup.find(id='course-details')
+  s_course_title = soup.find_all("h2", class_="course-title")
+  s_subject = soup.find_all("div", class_="subject-associations")
+  s_enrollment = soup.find_all("td", class_="class-enrollment-numbers nowrap")
+  s_section = soup.find_all("td", class_="class-section nowrap")
+  s_class_number = soup.find_all("td", class_="class-number nowrap")
 
-s_course_title = soup.find_all("h2", class_="course-title")
-s_subject = soup.find_all("div", class_="subject-associations")
-s_enrollment = soup.find_all("td", class_="class-enrollment-numbers nowrap")
-s_section = soup.find_all("td", class_="class-section nowrap")
-s_class_number = soup.find_all("td", class_="class-number nowrap")
+  text = []
+  for i in range(len(s_section)):
+    text.append((s_class_number[i], s_section[i], s_enrollment[i]))
 
-text = []
-for i in range(len(s_section)):
-  text.append((s_class_number[i], s_section[i], s_enrollment[i]))
+  sender_email = 'princetonnotifier@gmail.com'
+  password = 'Notifyme2020!'
+  receiver_email = "justincurl13@gmail.com"
 
-print("========== RESULTS ===========")
+  message = MIMEMultipart("alternative")
+  message["Subject"] = "[Enrollment Update] {}: {}".format(s_subject, s_course_title)
+  message["From"] = sender_email
+  message["To"] = receiver_email
 
-sections = results.find_all('td', class_='class-enrollment-numbers nowrap')
-for section in sections:
-  print(section)
+  # Create the plain-text and HTML version of your message
 
-sender_email = 'princetonnotifier@gmail.com'
-password = 'Notifyme2020!'
-receiver_email = "justincurl13@gmail.com"
+  html = """\
+  <html>
+    <body>
+      <h3> Class Information </h3>
+  """
+  for i in range(len(text)):
+    html+= """\
+      <ul>
+        <li>{}</li>
+        <li>{}</li>
+        <li>{}</li>
+      </ul>
+      <br>
+    """.format(text[i][0], text[i][1], text[i][2])
 
-message = MIMEMultipart("alternative")
-message["Subject"] = "{} Course Notification".format("Pol 316")
-message["From"] = sender_email
-message["To"] = receiver_email
+  html += """\
+    </body>
+  </html>
+  """
 
-# Create the plain-text and HTML version of your message
+  # Turn these into plain/html MIMEText objects
+  part1 = MIMEText(html, "html")
 
-html = """\
-<html>
-  <body>
-    <h3> Class Information </h3>
-"""
-for i in range(len(text)):
-  html+= """\
-    <ul>
-       <li>{}</li>
-       <li>{}</li>
-       <li>{}</li>
-    <ul>
-  """.format(text[i][0], text[i][1], text[i][2])
+  # Add HTML/plain-text parts to MIMEMultipart message
+  # The email client will try to render the last part first
+  message.attach(part1)
 
-html += """\
-  </body>
-</html>
-"""
-
-# Turn these into plain/html MIMEText objects
-part1 = MIMEText(html, "html")
-
-# Add HTML/plain-text parts to MIMEMultipart message
-# The email client will try to render the last part first
-message.attach(part1)
-
-# Create secure connection with server and send email
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(
-        sender_email, receiver_email, message.as_string()
-    )
+  # Create secure connection with server and send email
+  context = ssl.create_default_context()
+  with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+      server.login(sender_email, password)
+      server.sendmail(
+          sender_email, receiver_email, message.as_string()
+      )
